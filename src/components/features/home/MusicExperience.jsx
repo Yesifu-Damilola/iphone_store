d1e5a68680a171660ae8e819c488042d7f3aac7f
Yesifu-Damilola <yesifudamilolamax@gmail.com>
2025-02-27 04:52:04 +0100

    bug fixed on CartItem component

--- src/components/features/home/MusicExperience.jsx
+++ src/components/features/home/MusicExperience.jsx
@@ -14,36 +14,9 @@ export const MusicExperience = () => {
             Enhance Your Music Experience
           </p>
 
-          {/* <div className="flex flex-wrap justify-between mt-8">
-            <div className="flex w-1/2 md:w-1/4 justify-center mb-4">
-              <button className="text-[#000000] border rounded-full w-[62px] h-[62px] bg-white flex flex-col items-center justify-center">
-                <p className="text-base">23</p>
-                <p className="text-xs">Hours</p>
-              </button>
-            </div>
-            <div className="flex w-1/2 md:w-1/4 justify-center mb-4">
-              <button className="text-[#000000] border rounded-full w-[62px] h-[62px] bg-white flex flex-col items-center justify-center">
-                <p className="text-base">05</p>
-                <p className="text-xs">Days</p>
-              </button>
-            </div>
-            <div className="flex w-1/2 md:w-1/4 justify-center mb-4">
-              <button className="text-[#000000] border rounded-full w-[62px] h-[62px] bg-white flex flex-col items-center justify-center">
-                <p className="text-base">59</p>
-                <p className="text-xs">Minutes</p>
-              </button>
-            </div>
-            <div className="flex w-1/2 md:w-1/4 justify-center mb-4">
-              <button className="text-[#000000] border rounded-full w-[62px] h-[62px] bg-white flex flex-col items-center justify-center">
-                <p className="text-base">25</p>
-                <p className="text-xs">Seconds</p>
-              </button>
-            </div>
-          </div> */}
-
           <div className="py-4">
             <CountdownTimer
-              targetDate="2025-03-02T23:59:59"
+              targetDate="2025-03-30T23:59:59"
               className="text-white"
             />
           </div>
